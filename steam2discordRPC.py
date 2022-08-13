d68071b1fa7244c5f66b486711c9949ca63e6d34
Kesefon <kesefon@pm.me>
2022-08-13 14:20:37 +0200

    Add blacklist

--- steam2discordRPC.py
+++ steam2discordRPC.py
@@ -6,6 +6,8 @@ import requests
 
 client_id = '1007823600230879272'
 steam_id = '196062033'
+blacklist = ['945360']
+
 discord_rpc = Client(client_id)
 while True:
   try:
@@ -20,16 +22,20 @@ while True:
   try:
     print(response.json()["in_game"])
     appid = response.json()["in_game"]["logo"][46:-19]
-    print(appid) #TODO: appid based blacklist / override
-    state = response.json()["in_game"]["rich_presence"]
-    if state == "": state = None
-    discord_rpc.set_activity(
-      state = state,
-      large_image = response.json()["in_game"]["logo"],
-      details = response.json()["in_game"]["name"],
-      buttons = [{"label": "Open Store Page", "url": "https://store.steampowered.com/app/" + appid}, {"label": "Launch Game", "url": "steam://run/" + appid}]
-    )
-    time.sleep(30)
+    if appid in blacklist:
+      discord_rpc.clear_activity()
+      time.sleep(120)
+    else:
+      print(appid) #TODO: appid based override
+      state = response.json()["in_game"]["rich_presence"]
+      if state == "": state = None
+      discord_rpc.set_activity(
+        state = state,
+        large_image = response.json()["in_game"]["logo"],
+        details = response.json()["in_game"]["name"],
+        buttons = [{"label": "Open Store Page", "url": "https://store.steampowered.com/app/" + appid}, {"label": "Launch Game", "url": "steam://run/" + appid}]
+      )
+      time.sleep(30)
   except KeyError:
     discord_rpc.clear_activity()
     time.sleep(120)
